SVM fixed and feature normalisation added

Model/RNN-Model.py
@@ -42,6 +42,6 @@ testLable = np.array(testLable)
 
 print(trainLable[0:10])
 print(testLable[0:10])
-learningModel = LogisticRegresionModel(trainData, testData, trainLable, testLable)
+learningModel = SVMModel(trainData, testData, trainLable, testLable)
 learningModel.model()
 #print(trainData)

Model/SVMModel.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import math
 import matplotlib.pyplot as plt
+import os
 
 
 class SVMModel:
@@ -9,8 +10,8 @@ class SVMModel:
     def __init__(self, X_train, X_test, y_train, y_test):
         self.X_train = np.array(X_train)
         self.X_test = np.array(X_test)
-        self.y_train = np.array(y_train)
-        self.y_test = np.array(y_test)
+        self.y_train = np.array([1 if y == 0 else -1 for y in y_train])
+        self.y_test = np.array([1 if y == 0 else -1 for y in y_test])
         self.W = tf.Variable(tf.random_normal(
             shape=[self.X_train.shape[1],1]))
         self.b = tf.Variable(tf.random_normal(
@@ -24,28 +25,34 @@ class SVMModel:
 
         return X, Y
 
-    def model(self, run_count=500, alpha=0.001, learning_rate=0.1, batch_size=64):
+    def model(self, run_count=1000, alpha=0.02, learning_rate=0.01, batch_size=512):
         
         X, Y = self.create_placeholders()
 
-        #cost
         model_output = tf.subtract(tf.matmul(X, self.W), self.b)
-        l2_norm = tf.reduce_sum(tf.square(self.W))
-        alpha = tf.constant([alpha])
-        classification_term = tf.reduce_mean(tf.maximum(
-            0., tf.subtract(1., tf.multiply(model_output, Y))))
-        cost = tf.add(classification_term, tf.multiply(alpha, l2_norm))
 
-        #accuracy
-        model_output = tf.subtract(tf.matmul(X, self.W), self.b)
+        # Declare vector L2 'norm' function squared
+        l2_norm = tf.reduce_sum(tf.square(self.W))
+
+        # Declare loss function
+        # Loss = max(0, 1-pred*actual) + alpha * L2_norm(A)^2
+        # L2 regularization parameter, alpha
+        alpha = tf.constant([alpha])
+        # Margin term in loss
+        classification_term = tf.reduce_mean(tf.maximum(0., tf.subtract(1., tf.multiply(model_output, Y))))
+        # Put terms together
+        loss = tf.add(classification_term, tf.multiply(alpha, l2_norm))
+
+        # Declare prediction function
         prediction = tf.sign(model_output)
         accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, Y), tf.float32))
 
+        # Declare optimizer
+        my_opt = tf.train.AdamOptimizer(learning_rate)
+        train_step = my_opt.minimize(loss)
+
         # Initialize variables
         init = tf.global_variables_initializer()
-
-        my_opt = tf.train.GradientDescentOptimizer(learning_rate)
-        train_step = my_opt.minimize(cost)
         
         config = tf.ConfigProto(
                 device_count = {'GPU': 0}
@@ -62,9 +69,10 @@ class SVMModel:
                     len(self.X_train), size=batch_size)
                 rand_x = self.X_train[rand_index]
                 rand_y = np.transpose([self.y_train[rand_index]])
+
                 sess.run(train_step, feed_dict={X: rand_x, Y: rand_y})
 
-                temp_loss = sess.run(cost, feed_dict={
+                temp_loss = sess.run(loss, feed_dict={
                                      X: rand_x, Y: rand_y})
                 loss_vec.append(temp_loss)
 
@@ -78,13 +86,18 @@ class SVMModel:
                     Y: np.transpose([self.y_test])})
                 test_accuracy.append(test_acc_temp)
 
-                if (i + 1) % 100 == 0:
+                if (i + 1) % 10 == 0:
                     print('Step #{} test_acc = {}, train_acc = {}'.format(
                         str(i+1),
                         str(test_acc_temp),
                         str(train_acc_temp)
                     ))
                     print('Loss = ' + str(temp_loss))
+                if(test_acc_temp==max(test_accuracy) and test_acc_temp>0.8):
+                    dirname = os.path.dirname(__file__)
+                    np.savetxt(os.path.join(dirname,'W.out'), sess.run(self.W))
+                    np.savetxt(os.path.join(dirname,'b.out'), sess.run(self.b))
+
         # Extract coefficients
         # W_final = sess.run(self.W)
         # b_final = sess.run(self.b)